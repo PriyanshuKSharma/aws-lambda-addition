b9225b7b408f2e566749c5378b77dc8460a1e8aa
Priyanshu Kumar Sharma <priyanshu17ks@gmail.com>
2024-06-03 02:58:46 +0530

    Deleted unwanted files

--- lambda-function.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    num1 = int(event['num1'])
-    num2 = int(event['num2'])
-    result = num1 + num2
-    return {
-        'statusCode': 200,
-        'body': {'result': result}
-    }
