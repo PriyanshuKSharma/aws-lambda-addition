eac0c5d920c04d12bc735c6c27749ea3c6b4a999
Priyanshu Kumar Sharma <priyanshu17ks@gmail.com>
2024-06-03 02:11:54 +0530

    Wrote a function to add two numbers

--- lambda-function.py
+++ lambda-function.py
@@ -1,1 +1,8 @@
-print("Hello World")
+def lambda_handler(event, context):
+    num1 = int(event['num1'])
+    num2 = int(event['num2'])
+    result = num1 + num2
+    return {
+        'statusCode': 200,
+        'body': {'result': result}
+    }
